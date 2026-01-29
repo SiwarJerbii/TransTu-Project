b0ce377733dd2012b9aff62d7c3f8ad7cfcd9ff5
siwarjerbii <siwar.jerbi.tbs@gmail.com>
2026-01-29 21:04:49 +0100

    fixed URL Path Mismatch

--- run.py
+++ run.py
@@ -16,8 +16,8 @@ app = create_app()
 if __name__ == '__main__':
     
     host = os.getenv('API_HOST', '0.0.0.0')
-    port = int(os.environ.get("PORT", 5000)) 
-    debug = os.getenv('FLASK_DEBUG', 'True') == 'True'
+    port = int(os.environ.get("PORT", 5000))
+    debug = os.getenv('FLASK_DEBUG', 'False') == 'True'
     app.run(host="0.0.0.0", port=port)
     
     print("=" * 70)
